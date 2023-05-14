5c9f6428e715139bfe404c235598ab7b4c283670
Arin Soni <arinsoni1905@gmail.com>
2023-05-14 20:31:07 +0530

    Add files via upload

---
+++ police_siren_light.ino
@@ -0,0 +1,419 @@
+
+
+int blue = A4;
+int red = A5;
+int speaker =2;
+int t=60;
+int i=0;
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(2, OUTPUT); 
+  pinMode(A5, OUTPUT);
+  pinMode(A4, OUTPUT);
+}
+
+void loop() {
+   
+   digitalWrite(A4,HIGH);             // for police siren lights
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+ 
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+ 
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+  
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+
+   digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+ 
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+ 
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+  
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+
+   digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+ 
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+ 
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+  
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+
+   digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+ 
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+ 
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  
+  digitalWrite(A5,HIGH);
+  
+  delay(t);
+  
+  digitalWrite(A4,LOW);
+  
+  digitalWrite(A5,LOW);
+  
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+  
+  digitalWrite(A4,HIGH);
+  delay(t);
+  digitalWrite(A4,LOW);
+  digitalWrite(A5,HIGH);
+  delay(t);
+  digitalWrite(A5,LOW);
+  delay(t);
+
+
+    for(i=700;i<800;i++){   // for police siren
+  tone(2,i);
+  delay(15);
+  }
+  for(i=800;i>700;i--){
+  tone(2,i);
+  delay(15);
+  }
+  }
